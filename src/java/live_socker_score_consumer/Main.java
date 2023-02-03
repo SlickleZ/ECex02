dd55b9df2243b23c3ae9c966cfd9a5c91e2ee312
SlickleZ <kitikornfluke@gmail.com>
2023-02-03 14:08:07 +0700

    Initial src

---
+++ src/java/live_socker_score_consumer/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package live_socker_score_consumer;
+
+/**
+ *
+ * @author SlickleZ
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
