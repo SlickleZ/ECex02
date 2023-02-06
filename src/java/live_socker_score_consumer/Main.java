fd13dfd41313ebba09766463ab9117fb379ed475
SlickleZ <kitikornfluke@gmail.com>
2023-02-06 16:35:24 +0700

    Build live football score topic pub/sub

--- src/java/live_socker_score_consumer/Main.java
+++ src/java/live_socker_score_consumer/Main.java
@@ -5,12 +5,65 @@
  */
 package live_socker_score_consumer;
 
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
 /**
  *
  * @author SlickleZ
  */
 public class Main {
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    
+    
     public static void main(String[] args) {
+        Destination dest = null;
+        Connection connection = null;
         
+        try {
+            dest = (Destination) topic;
+        } catch (Exception e) {
+            System.err.println("Error setting destination: " + e.toString());
+            System.exit(1);
+        }
+        
+        try {
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer consumer = session.createConsumer(dest);
+            
+            connection.start();
+            
+            System.out.println("Welcome to live football score subscriber!");
+            
+            while (true) {
+                Message msg = consumer.receive();
+                
+                if (msg instanceof TextMessage) {
+                    TextMessage message = (TextMessage) msg;
+                    System.out.println("Updated!: " + message.getText());
+                }
+            }
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+            System.exit(1);
+        } finally {
+            try {
+                if (connection != null) { connection.close(); }
+            } catch (JMSException ex) {
+                System.err.println("Exception occurred when closing connection: " + ex.toString());
+            }
+        }
     }
 }
--- src/java/live_socker_score_producer/Main.java
+++ src/java/live_socker_score_producer/Main.java
@@ -5,17 +5,69 @@
  */
 package live_socker_score_producer;
 
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import java.util.Scanner;
+
 /**
  *
  * @author SlickleZ
  */
 public class Main {
-
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection connection = null;
+        Destination dest = null;
+        Scanner userListener;
+        
+        try {
+            dest = (Destination) topic;
+        } catch (Exception e) {
+            System.err.println("Error setting destination: " + e.toString());
+            System.exit(1);
+        }
+        
+        try {
+            userListener = new Scanner(System.in);
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(null);
+            TextMessage message = session.createTextMessage();
+            
+            System.out.println("Welcome to live score broadcast!");
+            
+            while(true) {
+                System.out.print("Enter the live score: ");
+                String userInput = userListener.nextLine();
+                
+                message.setText(userInput);
+                
+                producer.send(dest, message);
+                producer.send(dest, session.createMessage()); // ending message
+            }
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+        } finally {
+            try {
+                if (connection != null) { connection.close(); }
+            } catch (JMSException ex) {
+                System.err.println("Exception occurred when close connection: " + ex.toString());
+            }
+        }
     }
     
 }
